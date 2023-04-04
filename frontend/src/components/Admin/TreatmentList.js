fe593391e22207c092f5fcf8fade8f5130204281
fiona Banfa <fibsy79@yahoo.com>
2023-04-04 11:06:52 +0200

    commited

--- frontend/src/components/Admin/TreatmentList.js
+++ frontend/src/components/Admin/TreatmentList.js
@@ -11,7 +11,7 @@
  * 
  */
 
-/*import React from 'react'
+import React from 'react'
 
 function TreatmentList() {
   
@@ -133,4 +133,4 @@ function TreatmentList() {
     
 }
 
-export default TreatmentList */
+export default TreatmentList 
--- frontend/src/components/Admin/UserList.js
+++ frontend/src/components/Admin/UserList.js
@@ -9,11 +9,117 @@
  */
 
 import React from 'react'
-
 function UserList() {
+  
   return (
-    <div>UserList</div>
-  )
+    <div className="container">
+   <h1>List of Users</h1>
+   <table className="center">
+        <tbody>
+          <tr>
+            <th></th>
+            <th>NAME</th>
+            <th>EMAIL</th>
+            
+            
+            <th></th>
+          </tr>
+
+          {UserList.map((record) => (
+            <tr key={record._id}>
+              <td>{record.id}</td>
+
+              <td>
+                <input
+                  type="text"
+                  defaultValue={record.name}
+                  onChange={(evt) =>
+                    setUpdate({
+                      name: evt.target.value,
+                    })
+                  }
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  defaultValue={record.email}
+                  onChange={(evt) =>
+                    setUpdate({
+                      email: evt.target.value,
+                    })
+                  }
+                />
+              </td>
+             
+              
+              
+              
+
+              <td>
+                <input
+                  type="button"
+                  value="Delete"
+                  onClick={deleteList}
+                  id={record._id}
+                ></input>
+              </td>
+              <td>
+                <input
+                  type="button"
+                  value="Update"
+                  id={record._id}
+                  onClick={updateList}
+                ></input>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+     
+      <h1> Add new user</h1>
+       <div className="form-group">
+      
+       <form>
+      <label>Name</label>
+        
+      <input
+        
+        type="text"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <br />
+      
+      <label>Email</label>
+      
+      <input
+        
+        type="text"
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+      <br />
+      
+      
+    
+      
+      <button onClick={addTolist}>Add new user</button>
+      </form>
+      
+    </div>
+    
+    </div>
+  );
+
+
+
+
+    
 }
+
+
 
 export default UserList
