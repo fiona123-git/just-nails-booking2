be9402f6a4dd1736224acd61b8592655377d967a
fiona Banfa <fibsy79@yahoo.com>
2023-04-26 17:19:18 +0200

    made changes to facebook and google  login

--- controllers/user.js
+++ controllers/user.js
@@ -30,28 +30,6 @@ const authUser = asyncHandler(async (req, res) => {
 
 
 
-/*const authUser = asyncHandler(async (req, res) => {
-  const {email, password} = req.body
-const payload = jwt.verify(req.cookies.token, secret);
-  const user = await User.findOne(email)
-// match user to password
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      
-    isAdmin: user.isAdmin,
-    token: generateToken(payload.id)
-      
-    })
-  } else {
-    res.status(401)
-    throw new Error('Invalid email or password')
-  }
-}) */
-
-
 
 
 // @desc    Register a new user
